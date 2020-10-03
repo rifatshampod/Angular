7d5725faaf9c9118bc24515394accb8b55fb7420
rifatshampod <shampodrifat@gmail.com>
2020-10-03 10:10:34 +0800

    adding comments with food details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import {FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/comment.ts
@@ -0,0 +1,7 @@
+export class Comment{
+    id: string;
+    comment: string;
+    rating: string;
+    author: string;
+    date: Date;
+}
