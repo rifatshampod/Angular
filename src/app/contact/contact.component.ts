76200ddb613e09c71a62e0aa46c756765688c95d
rifatshampod <shampodrifat@gmail.com>
2020-10-20 08:33:50 +0800

    reactive form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validator} from '@angular/forms';
+import {Feedback, ContactType} from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType= ContactType;
+
+  constructor(private fb:FormBuilder) {
+    this.createForm();
+   }
 
   ngOnInit(): void {
+  }
+
+  createForm(){
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email:'',
+      agree:false,
+      contactType:'none',
+      message:''
+    });
+  }
+
+  onSubmit(){
+    this.feedback = this.feedbackForm.value;
+    this.feedbackForm.reset();
   }
 
 }
