1e96ebdcb3e9e01a499b3b6d8f3fa8582d32d3fe
rifatshampod <shampodrifat@gmail.com>
2020-10-27 08:48:34 +0800

    rxjs second

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -34,4 +34,8 @@ export class DishService {
     //     setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
     // });
   }
+
+  getDishIds(): Observable<string[] | any> {
+    return of(DISHES.map(dish => dish.id ));
+  }
 }
